3ca37b63a7dda21ef48170e9666de0dc5e1a9cbe
Никита <nikita.a.semenov@gmail.com>
2019-10-26 14:25:29 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Krasa on 26/10/2019.
+//  Copyright © 2019 Nikita Semenov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
